373658663f726e75f0031b27e2047805c397cf14
Federico C <fede-c89@hotmail.com>
2022-08-29 15:00:20 -0300

    commit 29-8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
-import PrimerComponente from './components/PrimerComponente';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from '../src/components/NavBar/NavBar'
 
 function App() {
@@ -8,19 +8,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <header className="App-header">
-        <PrimerComponente/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ItemListContainer/>
       </header>
     </div>
   );
